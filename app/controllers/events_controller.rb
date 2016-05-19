9b5ba21eef6ad105f7fb7e6590357356fd6cff49
Jon Perkins <jjperk@gmail.com>
2016-05-19 16:16:40 +0000

    Add user and event controller before actions to prevent non-logged in users from accessign indexes, creates, shows

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy, :update, :edit]
+  before_action :logged_in_user, only: [:create, :destroy, :update, :edit, :show, :new, :index]
   before_action :correct_user, only: [:destroy, :update, :edit]
   
   
